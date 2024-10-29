c2c244d7dde4503901a6a6e68a4376eeab36fa62
Nadia Siksnys <nadiasiksnys@gmail.com>
2024-10-29 20:15:00 -0300

    Added bonus search form component

---
+++ src/app/components/bonus/search-form/search-form.component.ts
@@ -0,0 +1,29 @@
+import { Component, EventEmitter, inject, Input, Output } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
+import { Salesman } from '../../../models/salesman.model';
+
+@Component({
+  selector: 'app-bonus-search-form',
+  standalone: true,
+  imports: [ ReactiveFormsModule ],
+  templateUrl: './search-form.component.html',
+})
+export class BonusSearchFormComponent {
+  formBuilder = inject(FormBuilder);
+  @Input() apiSalespeople: Salesman[] = [];
+  @Output() formSubmitted = new EventEmitter<any>();
+
+  form = this.formBuilder.group({
+    date: [ '', Validators.required ],
+    salespeople: [ '', Validators.required ]
+  });
+
+  submitForm() {
+    console.log(JSON.stringify(this.form.value));
+    this.formSubmitted.emit(this.form.value); 
+  }
+
+  resetForm() {
+    this.form.reset({ date: '', salespeople: '' });
+  }
+}
